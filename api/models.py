de0692a16f92e7368b93d184eb3447d275b4fd96
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-17 05:47:17 -0300

    Update urls.py

--- api/models.py
+++ api/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Gasto(models.Model):
     
-    criado = models.DateTimeField(auto_now_add=True)
+    criado = models.DateTimeField(auto_now_add=True) #VER COMO QUE DEIXA ISSO SEGURO QUE NAO POSSA SER ALTERADO
     categoria = models.CharField(max_length=100)
     quando = models.DateTimeField(auto_now_add=True)
     #quando = models.DateTimeField(default=datetime.now)
-    valor = models.DecimalField(max_digits=8, decimal_places=2, default=0)     #procurar melhor representação da moeda brasileira
+    valor = models.DecimalField(max_digits=8, decimal_places=2, default=0)  #MELHOR MANEIRA DE REPRESTENTAR A MOEDA BRASILEIRA
     descricao = models.TextField(blank=True)
 
     class Meta:
---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+#from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
+
+urlpatterns = [
+    path('gastos/', views.GastoList.as_view()),
+    path('gastos/<int:pk>', views.GastoDetail.as_view()),
+]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
