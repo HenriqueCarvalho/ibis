06c053115bb06d82d9a06162de19b7336e2a4c67
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-17 03:40:38 -0300

    created new model Gasto

--- api/models.py
+++ api/models.py
@@ -1,13 +1,12 @@
 from django.db import models
-#from datetime import datetime
 
 class Gasto(models.Model):
     
     criado = models.DateTimeField(auto_now_add=True)
+    categoria = models.CharField(max_length=100)
     quando = models.DateTimeField(auto_now_add=True)
     #quando = models.DateTimeField(default=datetime.now)
     valor = models.DecimalField(max_digits=8, decimal_places=2, default=0)     #procurar melhor representação da moeda brasileira
-    categoria = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
 
     class Meta:
