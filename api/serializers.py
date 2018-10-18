e9f631cbe3bf900a3db4fbcd6c0206467c661de9
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-18 03:10:02 -0300

    Update api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 from api.models import Gasto
+from api.models import TipoGasto
 
 class GastoSerializer(serializers.ModelSerializer):
 
+    #tipogasto = serializers.IntegerField(source='tipo.id', read_only=True)
+
     class Meta:
         model = Gasto
+        # 'categoria',
         fields = (
             'id',
             'criado',
-            'categoria',
+            'tipo',
             'quando',
             'valor',
             'descricao',
+        )
+
+class TipoGastoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TipoGasto
+        fields = (
+            'id',
+            'nome',
         )
