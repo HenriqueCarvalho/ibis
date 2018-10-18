577fa9784f2b03b654e1806e8ee92d860fd4e952
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-18 03:10:08 -0300

    Update api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from api import views
 urlpatterns = [
     path('gastos/', views.GastoList.as_view()),
     path('gastos/<int:pk>', views.GastoDetail.as_view()),
+    path('tipogastos/', views.TipoGastoList.as_view()),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
