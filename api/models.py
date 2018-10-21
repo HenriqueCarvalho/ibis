0aa01e83f97883a44afa335aed089d86057ad2fa
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-21 05:24:24 -0300

    Update api/models.py

--- api/models.py
+++ api/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class TipoGasto(models.Model):
     criado = models.DateTimeField(auto_now_add=True)
     nome = models.CharField(max_length=100)
-
+    usuario = models.ForeignKey('auth.User', related_name='tipogastos', on_delete=models.CASCADE) 
+         
     class Meta:
         #ordenação dos tipo
         ordering = ('nome',)
@@ -11,6 +12,7 @@ class TipoGasto(models.Model):
 class Gasto(models.Model):
     criado = models.DateTimeField(auto_now_add=True) #VER COMO QUE DEIXA ISSO SEGURO QUE NAO POSSA SER ALTERADO
     tipo = models.ForeignKey(TipoGasto, on_delete=models.CASCADE)
+    usuario = models.ForeignKey('auth.User', related_name='gastos', on_delete=models.CASCADE) 
     quando = models.DateTimeField(auto_now_add=True)
     valor = models.DecimalField(max_digits=8, decimal_places=2, default=0)  #MELHOR MANEIRA DE REPRESTENTAR A MOEDA BRASILEIRA
     descricao = models.TextField(blank=True)
@@ -19,16 +21,22 @@ class Gasto(models.Model):
         #ordenação dos gastos
         ordering = ('quando',)
 
-
-
 # Depois de criar o model roda as linhas de comando
 
 ##### python manage.py makemigrations [NOME DO APP]
 ##### python manage.py migrate
-
 
 # Populando no shell
 
 ##### python manage.py shell
 ##### from api.models import Gasto
 ##### from api.models import Gasto
+
+# Delete a table and start over
+##### rm -f db.sqlite3
+##### rm -r snippets/migrations
+##### python manage.py makemigrations [NOME DO APP]
+##### python manage.py migrate
+
+#new user
+#python manage.py createsuperuser
