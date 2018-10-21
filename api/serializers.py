ae2d02f17569a38d0c486bd3b2e88fb145aa4994
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-21 05:24:32 -0300

    Update api/serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -1,28 +1,49 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from api.models import Gasto
 from api.models import TipoGasto
 
+class UserSerializer(serializers.ModelSerializer):
+
+    gastos = serializers.PrimaryKeyRelatedField(many=True, queryset=Gasto.objects.all())
+    tipogastos = serializers.PrimaryKeyRelatedField(many=True, queryset=TipoGasto.objects.all())
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'gastos',
+            'tipogastos',
+        )
+
 class GastoSerializer(serializers.ModelSerializer):
 
-    #tipogasto = serializers.IntegerField(source='tipo.id', read_only=True)
+    usuario = serializers.CharField(source='usuario.username', read_only=True)
 
     class Meta:
         model = Gasto
-        # 'categoria',
         fields = (
             'id',
+            'usuario',
             'criado',
             'tipo',
             'quando',
             'valor',
             'descricao',
         )
+    
+    #def create(self, validated_data):
+        #return Gasto.objects.create(**validated_data)
 
 class TipoGastoSerializer(serializers.ModelSerializer):
+
+    usuario = serializers.CharField(source='usuario.username', read_only=True)
 
     class Meta:
         model = TipoGasto
         fields = (
             'id',
+            'usuario',
             'nome',
         )
