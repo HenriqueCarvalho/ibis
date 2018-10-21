f7ad1ff8751727c4d472a1485050ee2bfd5115b0
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-21 05:24:57 -0300

    Update api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
+    path('usuarios/', views.UserList.as_view()),
+    path('usuarios/<int:pk>/', views.UserDetail.as_view()),
     path('gastos/', views.GastoList.as_view()),
     path('gastos/<int:pk>', views.GastoDetail.as_view()),
     path('tipogastos/', views.TipoGastoList.as_view()),
+    path('tipogastos/<int:pk>', views.TipoGastoDetail.as_view()),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
