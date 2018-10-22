2abbe8e64726a85a03be67e845fd8935f91dd77b
HenriqueCarvalho <henrique.o.carvalho@hotmail.com.br>
2018-10-22 11:19:41 -0300

    Update ibis/settings.py

--- ibis/settings.py
+++ ibis/settings.py
@@ -100,11 +100,9 @@ DATABASES = {
 """
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(
-            default='postgres://zbtfhdrzkbldqa:c58802588678c917eba1967b5c43829a7e7342c5071c34176ccc72a11653c692@ec2-54-204-14-96.compute-1.amazonaws.com:5432/d8rhn4ustfjk0j'
-        )
-    }
+    'default':dj_database_url.config(
+        default='postgres://zbtfhdrzkbldqa:c58802588678c917eba1967b5c43829a7e7342c5071c34176ccc72a11653c692@ec2-54-204-14-96.compute-1.amazonaws.com:5432/d8rhn4ustfjk0j'
+    )
 }
 
 # Password validation
